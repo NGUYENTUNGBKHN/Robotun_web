5293d7d843d5f56dc68e450eefa0cec5e3fddca1
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2026-03-27 10:38:35 +0900

    post new

---
+++ posts/mcu/Dev-cortex-m-on-vscode.js
@@ -0,0 +1,23 @@
+POSTS_DATA.push({
+  id: "Dev-cortex-m-on-vscode",
+  title: "Develop (Compile/Debug) Cortex-M Series On Vscode",
+  excerpt: "Develop (Compile/Debug) Cortex-M Series On Vscode",
+  date: "2026-03-27",
+  author: "Gentantun",
+  tags: ["stm32", "mcu", "vscode"],
+  content: `## Prerequisite
+
+### Toolchain :
+
+### MinGW-GNU :
+
+### Cmake :
+
+### Vscode :
+
+### Terminal : 
+
+## Compile
+
+## Debug`
+});
