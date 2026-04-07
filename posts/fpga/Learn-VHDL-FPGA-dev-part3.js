d984cc7eab069b270e2f1ed82efd4a9ec7549ccd
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2026-04-07 22:35:33 +0900

    fix 2->3

--- posts/fpga/Learn-VHDL-FPGA-dev-part3.js
+++ posts/fpga/Learn-VHDL-FPGA-dev-part3.js
@@ -1,7 +1,7 @@
 POSTS_DATA.push({
   id: "Learn-VHDL-FPGA-dev-part3",
-  title: "Learn the fundamentals of VHDL and FPGA Develepment Part-2",
-  excerpt: "Learn the fundamentals of VHDL and FPGA development Part-2 - ButtonLED",
+  title: "Learn the fundamentals of VHDL and FPGA Develepment Part-3",
+  excerpt: "Learn the fundamentals of VHDL and FPGA development Part-3 - ButtonLED",
   date: "2026-04-07",
   author: "Gentantun",
   tags: ["fpga", "Cyclone-V"],
