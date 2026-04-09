330fc227ee7ab1679bb8c63125c84e77b6840dff
nguyen-thanh-tung <nguyenthanhtung8196@gmail.com>
2026-04-09 21:26:12 +0900

    update learn vhdl part4

---
+++ posts/fpga/Learn-VHDL-FPGA-dev-part4.js
@@ -0,0 +1,9 @@
+POSTS_DATA.push({
+  id: "Learn-VHDL-FPGA-dev-part4",
+  title: "Learn the fundamentals of VHDL and FPGA Develepment Part-4",
+  excerpt: "Learn the fundamentals of VHDL and FPGA development, Part 4 - Constant and Signals",
+  date: "2026-04-09",
+  author: "Gentantun",
+  tags: ["fpga", "Cyclone-V", "vhdl"],
+  content: `## Constant and Signals`
+});
